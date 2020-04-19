29e1df6df4fb3b46c9e8c7fcf74b3dad7de99dc2
mt-0509 <ppsr5504@yahoo.co.jp>
2020-04-19 21:26:05 +0900

    値をランダムで取得して結果を表示

--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
 'use strict';
 
 {
-  // 自分の値を取得する
-  let mine = document.getElementById('mine-attack');
-  mine.innerHTML = 
+  
+  function attack() {
+    return Math.floor(Math.random() * 100);
+  }
+  
+  // バトルを開始する
+  document.getElementById('battle').addEventListener('click', () => {
 
-  // 自分の攻撃力をランダムで決める
+    attack();
+    
+    // 自分の攻撃力をランダムで決める
+    let mine = document.getElementById('mine-attack');
+    let myattack = attack();
+    mine.innerHTML = myattack;
+  
+  
+    // 相手の攻撃力をランダムで決める
+    let cup = document.getElementById('cup-attack');
+    let cupattack = attack();
+    cup.innerHTML = cupattack;
+
+    // 結果を表示する
+    function result () {
+      if (myattack > cupattack) {
+        alert('あなたの勝ちです!');
+      } else {
+        alert('あなたの負けです!');
+      }
+    }
+    setTimeout(result, 1000);
+  })
   
 
 }
